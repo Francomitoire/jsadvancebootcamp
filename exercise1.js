33148a8adf7a00bb404aec1b933707a2c51230ad
Santiago Barua <santiago.barua@gm2dev.com>
2026-08-04 10:23:53 -0300

    chore: add Promises functions and Utilities of Promise.all and Promise.allSettled

--- exercise1.js
+++ exercise1.js
@@ -1,48 +1,29 @@
-// cosme fulanito
-
+//Santiago Barua
 function loadA() {
-  return new Promise((resolve) => setTimeout(() => resolve("A ok"), 1000));
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve('A valido');
+        }, 1000);
+    });
 }
-
 function loadB() {
-  return new Promise((resolve) => setTimeout(() => resolve("B ok"), 2000));
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve('B valido');
+        }, 2000);
+    });
 }
-
 function loadC() {
-  return new Promise((_, reject) => setTimeout(() => reject("C failed"), 1500));
-}
-
-Promise.all([loadA(), loadB(), loadC()])
-  .then((results) => console.log("all resueltas:", results))
-  .catch((error) => console.log("all catch:", error));
-
-Promise.allSettled([loadA(), loadB(), loadC()])
-  .then((results) => console.log("allSettled:", results))
-  .catch(() => console.log("esto nunca corre"));
-
-
-// Luciano Gonzalez
-function loadA() {
-    return new Promise((resolve) => {
-    setTimeout(resolve("bien1"), 1000)
-    })
-}
-function loadB() {
-    return new Promise((resolve) => {
-    setTimeout(resolve("bien2"), 2000)
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject(new Error('C fallo'));
+        }, 1500);
     });
 }
 
-function loadC() {
-    return new Promise((resolve ,reject) => {
-    setTimeout(reject("error"), 1500)
-    })
-}
-
 Promise.all([loadA(), loadB(), loadC()])
-    .catch((reason) => console.log(reason))
-
+    .then((values) => console.log(values))
+    .catch((error) => console.error(error.message));
 
 Promise.allSettled([loadA(), loadB(), loadC()])
-    .then((values) => console.log(values))
-    .catch((reason) =>console.log(reason))
+    .then((values) => console.log(values));
