1be3d2a7465c11ca28dac6ee4b95ebdccb6407ac
mayragm2 <mayracueto@gm2dev.com>
2026-08-04 12:52:18 -0300

    feat(exercise2): add promise.race between the timeout and work

--- exercise2.js
+++ exercise2.js
@@ -1,4 +1,19 @@
-function work() { /* resuelve a ~2000ms */ }
+function work () {
+    return new Promise ((resolve) => {
+        setTimeout(() => {
+            resolve("Work Done");
+        }, 2000
+        );
+    })
+};
+
 const timeout = (ms) => new Promise((_, reject) =>
   setTimeout(() => reject(new Error("timed out")), ms)
 );
+
+Promise.race([work(), timeout(1000)])
+.then ((values) => console.log(values))
+.catch((error) => console.log(error));
+
+Promise.race([work(), timeout(2500)])
+.then ((values) => console.log(values));
