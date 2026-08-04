fca2d5ee2313849111b470763b0e8d7b9d2a56b2
JeremiasSosaa <jeremias.sosa@gm2dev.com>
2026-08-04 10:36:25 -0300

    chore(complete exercise1): complete exercise of the promises
    Ejercicio completo sobre el promise.all y el promise.allSettled

--- exercise1.js
+++ exercise1.js
@@ -1,22 +1,29 @@
-// cosme fulanito
+// cosme fulanito // Jeremias Sosa 
 
 function loadA() {
-  return new Promise((resolve) => setTimeout(() => resolve("A ok"), 1000));
+    return new Promise((resolve) => setTimeout(resolve, 1000, 'A ok'))
 }
-
 function loadB() {
-  return new Promise((resolve) => setTimeout(() => resolve("B ok"), 2000));
+    return new Promise((resolve) => setTimeout(resolve, 2000, 'B ok'))
 }
-
 function loadC() {
-  return new Promise((_, reject) => setTimeout(() => reject("C failed"), 1500));
+    return new Promise((resolve, reject) => setTimeout(reject, 1500, 'C not ok'))
 }
 
 Promise.all([loadA(), loadB(), loadC()])
-  .then((results) => console.log("all resueltas:", results))
-  .catch((error) => console.log("all catch:", error));
+    .then((results) => console.log("Todas ok:", results))
+    .catch((error) => console.log("Catch:", error)); // "C not ok"
 
 Promise.allSettled([loadA(), loadB(), loadC()])
+    .then((results) => {
+        results.forEach((resultsSettled) => {
+            if (resultsSettled.status === "fulfilled") {
+                console.log(`Resuelta: ${resultsSettled.value}`); // te da 'resuelta: A' 'resuelta: B'
+            } else {
+                console.log(`Rechazada: ${resultsSettled.value}, error: ${resultsSettled.reason}`); //  te da 'Rechazada: C Error: razon de por que'
+            }
+        });
+    });
   .then((results) => console.log("allSettled:", results))
   .catch(() => console.log("esto nunca corre"));
 
