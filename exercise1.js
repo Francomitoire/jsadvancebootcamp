52e4b8de2a55ffa271d427e325701a3fb3828754
mateo <mateo.roza@gm2dev.com>
2026-08-04 10:35:09 -0300

    feat(exercise1.js)add:implemented examples using promise.all and promise.allSettled

--- exercise1.js
+++ exercise1.js
@@ -1,1 +1,20 @@
 // Cosme fulanito
+
+
+function cargarA() {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve("A"), 1000);
+  });
+}
+
+function cargarB() {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve("B"), 2000);
+  });
+}
+
+function cargarC() {
+  return new Promise((_, reject) => {
+    setTimeout(() => reject(new Error("C fallo")), 1500);
+  });
+}
