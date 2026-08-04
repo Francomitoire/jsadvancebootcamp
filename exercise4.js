bf4901a950e773a3e23b1cba36fb70290c5abe8d
Santiago Barua <santiago.barua@gm2dev.com>
2026-08-04 12:45:05 -0300

    feat(exercise4.js): Add exercise4.js to main

--- exercise4.js
+++ exercise4.js
@@ -1,43 +1,28 @@
+//Santiago Barua
 function loadA() {
-    return new Promise((resolve) => setTimeout(resolve, 1000, 'Tobias'))
+    return new Promise((resolve) => setTimeout(resolve, 1000, 'Paso a'))
 }
 
 function loadB() {
-    return new Promise((resolve) => setTimeout(resolve, 2000, 'Giovanni'))
+    return new Promise((resolve) => setTimeout(resolve, 2000, 'Paso b'))
 }
 
 function loadCFail() {
-    return new Promise((resolve, reject) => setTimeout(reject, 1500, 'Falto Barua'))
+    return new Promise((resolve, reject) => setTimeout(reject, 1500, 'Fallo c'))
 }
 
 function loadC() {
-    return new Promise((resolve) => setTimeout(resolve, 1500, 'Barua'))
+    return new Promise((resolve) => setTimeout(resolve, 1500, 'Paso c'))
 }
 
 function loadD() {
-    return new Promise((resolve) => setTimeout(resolve, 1500, 'Mayra'))
+    return new Promise((resolve) => setTimeout(resolve, 1500, 'Paso d'))
 }
 
-function loadE() {
-    return new Promise((resolve) => setTimeout(resolve, 1500, 'Juli'))
-}
+Promise.all([loadA(), loadB(), loadCFail(), loadD()])
+    .then((results) => console.log(results))
+    .catch((error) => console.log("Error:", error));
 
-function loadF() {
-    return new Promise((resolve) => setTimeout(resolve, 1500, 'Mateo'))
-}
-
-function loadG() {
-    return new Promise((resolve) => setTimeout(resolve, 1500, 'Gonza'))
-}
-
-function loadH() {
-    return new Promise((resolve) => setTimeout(resolve, 1500, 'Jere'))
-}
-
-Promise.all([loadA(), loadB(), loadCFail(), loadD(), loadE(), loadF(), loadG(), loadH()])
-    .then((results) => console.log("Todas ok:", results))
-    .catch((error) => console.log("Catch:", error));
-
-Promise.all([loadA(), loadB(), loadC(), loadD(), loadE(), loadF(), loadG(), loadH()])
-    .then((results) => console.log("Todas ok:", results))
-    .catch((error) => console.log("Catch:", error));
+Promise.all([loadA(), loadB(), loadC(), loadD()])
+    .then((results) => console.log(results))
+    .catch((error) => console.log("Error:", error));
