74921cdfbc7ae02aa60cd362d5fda1027dd8a1fe
mayragm2 <mayracueto@gm2dev.com>
2026-08-04 10:41:03 -0300

    feat(exercise1): add promises and combine them using all and allSettled. Print result in console.

--- exercise1.js
+++ exercise1.js
@@ -45,3 +45,25 @@ Promise.all([loadA(), loadB(), loadC()])
 Promise.allSettled([loadA(), loadB(), loadC()])
     .then((values) => console.log(values))
     .catch((reason) =>console.log(reason))
+
+// Mayra Cueto
+
+const promise1 = new Promise((resolve) => {
+  setTimeout(() => resolve("Promise 1 resolved"), 1000);
+});
+
+const promise2 = new Promise((resolve) => {
+  setTimeout(() => resolve("Promise 2 resolved"), 2000);
+});
+
+const promise3 = new Promise((_, reject) => {
+  setTimeout(() => reject("Promise 3 rejected"), 1500);
+});
+
+Promise.all([promise1, promise2, promise3])
+    .then((values) => console.log(values))
+    .catch((error) => console.log("error"));    
+
+Promise.allSettled([promise1, promise2, promise3])
+    .then((values) => console.log(values))
+    .catch((error) => console.log("error 2"));    
