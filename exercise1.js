c9b4d31344ceb6c843f46e0de616473878d05bca
tobiasmanquez <tobias.manquez@gm2dev.com>
2026-08-04 11:01:31 -0300

    (fix): add the call to action (all) and (allsettled)

--- exercise1.js
+++ exercise1.js
@@ -1,1 +1,29 @@
-// cosme fulanito
+//Tobias Manquez
+
+
+function loadA() { 
+return new Promise((resolve) => setTimeout(() => resolve("Promise A resolved"), 1000));
+}
+
+function loadB() { 
+return new Promise((resolve) => setTimeout(() => resolve("Promise B resolved"), 2000));
+}
+
+function loadC() { 
+return new Promise((_, reject) => setTimeout(() => reject("Promise C rejected"), 1500));
+    
+}
+
+Promise.all([loadA(), loadB(), loadC()])
+    .then((resultados) => {
+        console.log("Todo se resolvió:", resultados);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+Promise.allSettled([loadA(), loadB(), loadC()])
+    .then((resultados) => {
+        console.log("Resultados de todas las promesas:", resultados);
+    });
+
