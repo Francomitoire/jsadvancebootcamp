dfbb9301333008684b1869e0a885d08fb9cf38e1
Luciano <Luciano.gonzalez@gm2.com>
2026-08-04 09:59:01 -0300

    feat(exercises1.js)add:implement examples using Promise.all and Promise.allSettled

--- exercise1.js
+++ exercise1.js
@@ -45,3 +45,29 @@ Promise.all([loadA(), loadB(), loadC()])
 Promise.allSettled([loadA(), loadB(), loadC()])
     .then((values) => console.log(values))
     .catch((reason) =>console.log(reason))
+
+// Luciano Gonzalez
+function loadA() {
+    return new Promise((resolve) => {
+    setTimeout(resolve("bien1"), 1000)
+    })
+}
+function loadB() {
+    return new Promise((resolve) => {
+    setTimeout(resolve("bien2"), 2000)
+    });
+}
+
+function loadC() {
+    return new Promise((resolve ,reject) => {
+    setTimeout(reject("error"), 1500)
+    })
+}
+
+Promise.all([loadA(), loadB(), loadC()])
+    .catch((reason) => console.log(reason))
+
+
+Promise.allSettled([loadA(), loadB(), loadC()])
+    .then((values) => console.log(values))
+    .catch((reason) =>console.log(reason))
