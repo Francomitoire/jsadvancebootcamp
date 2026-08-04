132b6add51125cda3c7318d8a800856cef1861b5
Julieta <julieta.insaurralde@gm2dev.com>
2026-08-04 10:20:05 -0300

    feat: implement promise functions and usage examples

--- exercise1.js
+++ exercise1.js
@@ -1,1 +1,21 @@
-// cosme fulanito
+// Julieta 
+
+function loadA(){
+     return new Promise ((resolve) =>{
+    setTimeout(() => resolve("Promise 1 resolved"),1000);
+});}
+function loadB(){
+   return new Promise((resolve)=>{
+    setTimeout(()=> resolve("promise 2 resolved"), 2000);
+});}
+function loadC(){ 
+   return new Promise ((reject)=> {
+    setTimeout(() => reject("promise 3 rejected"), 1500);
+})}
+
+Promise.all([loadA(), loadB(), loadC()])
+ .then((results)=>{ console.log(results)})
+    .catch((error)=>{ console.log(error)})
+
+Promise.allSettled([loadA(), loadB(), loadC()])
+    .then((results)=>{ console.log(results)})
