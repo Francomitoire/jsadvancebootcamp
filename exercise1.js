2545c05195c9decf06a06502963f7673e958b1f1
Francomitoire <franco.mitoire@gm2dev.com>
2026-08-04 10:48:04 -0300

    chore: reset file

--- exercise1.js
+++ exercise1.js
@@ -1,54 +1,25 @@
-// cosme fulanito // Jeremias Sosa 
+// cosme fulanito
 
 function loadA() {
-    return new Promise((resolve) => {
-    setTimeout(resolve("bien1"), 1000)
-    })
+  return new Promise((resolve) => setTimeout(() => resolve("A ok"), 1000));
 }
+
 function loadB() {
-    return new Promise((resolve) => {
-    setTimeout(resolve("bien2"), 2000)
-    });
+  return new Promise((resolve) => setTimeout(() => resolve("B ok"), 2000));
 }
+
 function loadC() {
-    return new Promise((resolve ,reject) => {
-    setTimeout(reject("error"), 1500)
-    })
+  return new Promise((_, reject) => setTimeout(() => reject("C failed"), 1500));
 }
 
 Promise.all([loadA(), loadB(), loadC()])
-    .catch((reason) => console.log(reason))
-
-
-Promise.allSettled([loadA(), loadB(), loadC()])
-    .then((values) => console.log(values))
-    .catch((reason) =>console.log(reason))
- // Jeremias Sosa 
-
-function loadA() {
-    return new Promise((resolve) => setTimeout(resolve, 1000, 'A ok'))
-}
-function loadB() {
-    return new Promise((resolve) => setTimeout(resolve, 2000, 'B ok'))
-}
-function loadC() {
-    return new Promise((resolve, reject) => setTimeout(reject, 1500, 'C not ok'))
-}
-
-Promise.all([loadA(), loadB(), loadC()])
-    .then((results) => console.log("Todas ok:", results))
-    .catch((error) => console.log("Catch:", error)); // "C not ok"
+  .then((results) => console.log("all resueltas:", results))
+  .catch((error) => console.log("all catch:", error));
 
 Promise.allSettled([loadA(), loadB(), loadC()])
-    .then((results) => {
-        results.forEach((resultsSettled) => {
-            if (resultsSettled.status === "fulfilled") {
-                console.log(`Resuelta: ${resultsSettled.value}`); // te da 'resuelta: A' 'resuelta: B'
-            } else {
-                console.log(`Rechazada: ${resultsSettled.value}, error: ${resultsSettled.reason}`); //  te da 'Rechazada: C Error: razon de por que'
-            }
-        });
-    });
+  .then((results) => console.log("allSettled:", results))
+  .catch(() => console.log("esto nunca corre"));
+
 // Luciano Gonzalez
 function loadA() {
     return new Promise((resolve) => {
