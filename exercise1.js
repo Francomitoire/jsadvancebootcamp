e90a1fdd68fdcc61d14dbfc0f359078df8969c36
mayragm2 <mayracueto@gm2dev.com>
2026-08-04 11:25:01 -0300

    chore(exercise1): comment old main code and insert new working code

--- exercise1.js
+++ exercise1.js
@@ -1,26 +1,49 @@
 // cosme fulanito // Jeremias Sosa 
 
-function loadA() {
-    return new Promise((resolve) => setTimeout(resolve, 1000, 'A ok'))
-}
-function loadB() {
-    return new Promise((resolve) => setTimeout(resolve, 2000, 'B ok'))
-}
-function loadC() {
-    return new Promise((resolve, reject) => setTimeout(reject, 1500, 'C not ok'))
-}
+//function loadA() {
+//    return new Promise((resolve) => setTimeout(resolve, 1000, 'A ok'))
+//}
+//function loadB() {
+//    return new Promise((resolve) => setTimeout(resolve, 2000, 'B ok'))
+//}
+//function loadC() {
+//    return new Promise((resolve, reject) => setTimeout(reject, 1500, 'C not ok'))
+//}
+//
+//Promise.all([loadA(), loadB(), loadC()])
+//    .then((results) => console.log("Todas ok:", results))
+//    .catch((error) => console.log("Catch:", error)); // "C not ok"
+//
+//Promise.allSettled([loadA(), loadB(), loadC()])
+//    .then((results) => {
+//        results.forEach((resultsSettled) => {
+//            if (resultsSettled.status === "fulfilled") {
+//                console.log(`Resuelta: ${resultsSettled.value}`); // te da 'resuelta: A' 'resuelta: B'
+//            } else {
+//                console.log(`Rechazada: ${resultsSettled.value}, error: ${resultsSettled.reason}`); //  te da 'Rechazada: C Error: razon de por que'
+//            }
+//        });
+//    });
+//
+//
+// Mayra Cueto
 
-Promise.all([loadA(), loadB(), loadC()])
-    .then((results) => console.log("Todas ok:", results))
-    .catch((error) => console.log("Catch:", error)); // "C not ok"
+const promise1 = new Promise((resolve) => {
+  setTimeout(() => resolve("Promise 1 resolved"), 1000);
+});
 
-Promise.allSettled([loadA(), loadB(), loadC()])
-    .then((results) => {
-        results.forEach((resultsSettled) => {
-            if (resultsSettled.status === "fulfilled") {
-                console.log(`Resuelta: ${resultsSettled.value}`); // te da 'resuelta: A' 'resuelta: B'
-            } else {
-                console.log(`Rechazada: ${resultsSettled.value}, error: ${resultsSettled.reason}`); //  te da 'Rechazada: C Error: razon de por que'
-            }
-        });
-    });
+const promise2 = new Promise((resolve) => {
+  setTimeout(() => resolve("Promise 2 resolved"), 2000);
+});
+
+const promise3 = new Promise((_, reject) => {
+  setTimeout(() => reject("Promise 3 rejected"), 1500);
+});
+
+Promise.all([promise1, promise2, promise3])
+    .then((values) => console.log(values))
+    .catch((error) => console.log("error"));    
+
+Promise.allSettled([promise1, promise2, promise3])
+    .then((values) => console.log(values))
+    .catch((error) => console.log("error 2"));    
