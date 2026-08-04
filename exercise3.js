ae2e121d898c350fe3045e6d68ea3478bb9fc725
Santiago Barua <santiago.barua@gm2dev.com>
2026-08-04 11:58:53 -0300

    feat: Implement Promise.any with multiple mirror & error functions

---
+++ exercise3.js
@@ -0,0 +1,45 @@
+function mirrorA() {
+  return new Promise((_, reject) => {
+    setTimeout(() => reject(new Error("mirror A failed")), 300);
+  });
+}
+function mirrorB() {
+  return new Promise((_, reject) => {
+    setTimeout(() => reject(new Error("mirror B failed")), 500);
+  });
+}
+function mirrorC() {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve("mirror C data"), 800);
+  });
+}
+
+Promise.any([mirrorA(), mirrorB(), mirrorC()])
+  .then((value) => console.log(value))
+  .catch((error) => console.error(error));
+
+function mirrorAFails() {
+  return new Promise((_, reject) => {
+    setTimeout(() => reject(new Error("mirror A failed")), 300);
+  });
+}
+function mirrorBFails() {
+  return new Promise((_, reject) => {
+    setTimeout(() => reject(new Error("mirror B failed")), 500);
+  });
+}
+function mirrorCFails() {
+  return new Promise((_, reject) => {
+    setTimeout(() => reject(new Error("mirror C failed")), 800);
+  });
+}
+
+Promise.any([mirrorAFails(), mirrorBFails(), mirrorCFails()])
+  .then((value) => console.log(value))
+  .catch((error) => {
+    if (error instanceof AggregateError) {
+      console.error(error.errors);
+    } else {
+      console.error(error);
+    }
+  });
