dd41edc6702b473c2923cb1ad3bb27a66cf2af96
tobiasmanquez <tobias.manquez@gm2dev.com>
2026-08-04 12:15:27 -0300

    Comment out promise functions and their usage in exercise1.js

--- exercise1.js
+++ exercise1.js
@@ -129,22 +129,24 @@ Promise.allSettled([promise1, promise2, promise3])
     .catch((error) => console.log("error 2"));    
 */
 
-function loadA(){
-     return new Promise ((resolve) =>{
-    setTimeout(() => resolve("Promise 1 resolved"),1000);
-});}
-function loadB(){
-   return new Promise((resolve)=>{
-    setTimeout(()=> resolve("promise 2 resolved"), 2000);
-});}
-function loadC(){ 
-   return new Promise ((reject)=> {
-    setTimeout(() => reject("promise 3 rejected"), 1500);
-})}
+//Gonzalo
 
-Promise.all([loadA(), loadB(), loadC()])
- .then((results)=>{ console.log(results)})
-    .catch((error)=>{ console.log(error)})
+// function loadA(){
+//      return new Promise ((resolve) =>{
+//     setTimeout(() => resolve("Promise 1 resolved"),1000);
+// });}
+// function loadB(){
+//    return new Promise((resolve)=>{
+//     setTimeout(()=> resolve("promise 2 resolved"), 2000);
+// });}
+// function loadC(){ 
+//    return new Promise ((reject)=> {
+//     setTimeout(() => reject("promise 3 rejected"), 1500);
+// })}
 
-Promise.allSettled([loadA(), loadB(), loadC()])
-    .then((results)=>{ console.log(results)})
+// Promise.all([loadA(), loadB(), loadC()])
+//  .then((results)=>{ console.log(results)})
+//     .catch((error)=>{ console.log(error)})
+
+// Promise.allSettled([loadA(), loadB(), loadC()])
+//     .then((results)=>{ console.log(results)})
