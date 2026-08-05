862820bfd0758df64d7a2ab6aa8fc72f385d2013
tobiasmanquez <tobias.manquez@gm2dev.com>
2026-08-05 12:53:49 -0300

    refactor: update work and timeout functions for clarity and consistency

--- exercise2.js
+++ exercise2.js
@@ -71,23 +71,39 @@ Promise.race([work(), timeout(2500)])
 //  .catch((error) => console.error(error.message));
 */
 
-//Mayra Cueto
-function work () {
-    return new Promise ((resolve) => {
-        setTimeout(() => {
-            resolve("Work Done");
-        }, 2000
-        );
-    })
-};
+// //Mayra Cueto
+// function work () {
+//     return new Promise ((resolve) => {
+//         setTimeout(() => {
+//             resolve("Work Done");
+//         }, 2000
+//         );
+//     })
+// };
 
-const timeout = (ms) => new Promise((_, reject) =>
-  setTimeout(() => reject(new Error("timed out")), ms)
+// const timeout = (ms) => new Promise((_, reject) =>
+//   setTimeout(() => reject(new Error("timed out")), ms)
+// );
+
+// Promise.race([work(), timeout(1000)])
+// .then ((values) => console.log(values))
+// .catch((error) => console.log(error));
+
+// Promise.race([work(), timeout(2500)])
+// .then ((values) => console.log(values));
+
+// Tobias Manquez 
+
+function work() {
+    return new Promise((resolve) => { setTimeout(() => resolve("work done"), 2000); });
+}
+const timeout = (ms) => new Promise((_, reject) => setTimeout(() => reject(new Error("timed out")), ms)
 );
 
 Promise.race([work(), timeout(1000)])
-.then ((values) => console.log(values))
-.catch((error) => console.log(error));
+    .then((value) => console.log(value))
+    .catch((error) => console.error(error.message));
 
 Promise.race([work(), timeout(2500)])
-.then ((values) => console.log(values));
+    .then((value) => console.log(value))
+    .catch((error) => console.error(error.message));
