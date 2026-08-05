14a8d9733536bede10e7c12e88ad0a1d8121a2d5
Giovanni La Volpe <giovanni.lavolpe@gm2dev.com>
2026-08-05 11:05:53 -0300

    feat(exercise1.js) add code for exercise 1

---
+++ race-conditions/exercise1.js
@@ -0,0 +1,25 @@
+let counter = 0;
+
+function incrementCounterGiovanni() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+        if (counter < 4) {
+        counter = counter + 1;
+        resolve(); }
+        else {
+              console.log(`Listo: ${counter}`);
+        }
+    }, Math.random() * 200);
+  });
+}
+
+async function incrementSecuentiallyGiovanni() {
+  await incrementCounterGiovanni(); // 0 a 1
+  await incrementCounterGiovanni(); // 1 a 2
+  await incrementCounterGiovanni(); // 2 a 3
+  await incrementCounterGiovanni(); // 3 a 4
+
+  console.log(`Listo: ${counter}`);
+}
+
+incrementSecuentiallyGiovanni();
