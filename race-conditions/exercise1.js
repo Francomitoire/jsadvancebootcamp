cf7f7a3aa76d66783ced9845fb8eb3ffd48744be
JeremiasSosaa <jeremias.sosa@gm2dev.com>
2026-08-05 10:55:11 -0300

    feat(exercise1.js):add my solution of the exercise 1 - Sosa

--- race-conditions/exercise1.js
+++ race-conditions/exercise1.js
@@ -1,3 +1,26 @@
+let counter = 0;
+
+function incrementCounterJere() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            const currentValueJere = counterJere;
+            setTimeout(() => {
+                counterJere = currentValueJere + 1;
+                resolve();
+            }, 200);
+        }, Math.random() * 200);
+    });
+}
+
+async function incrementSequentiallyJere(numberJere) {
+    for (let i = 0; i < numberJere; i++) {
+        await incrementCounterJere();
+    }
+    console.log(`Listo: ${counterJere}`);
+}
+
+incrementSequentiallyJere(4);
+
 /*
 // Tobias Manquez
 
