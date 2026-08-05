ed609b0a134ed8910878c56a0a25bfd0b7b273ae
julietainsaurraldegm2 <julieta.insaurralde@gm2dev.com>
2026-08-05 11:13:19 -0300

    test

--- race-conditions/exercise1.js
+++ race-conditions/exercise1.js
@@ -90,3 +90,26 @@ incrementSequentiallytobias();
 //    .then (() => incrementCounterMay())
 //    .then (() => incrementCounterMay())
 //    .then (() => console.log(counterMay));
+
+/*let counter = 0;
+function incrementCounterJulieta() {
+    return new Promise((resolve) =>{
+    setTimeout(() => {
+    const currentValue = counter;
+    setTimeout(() => {
+      counter = currentValue + 1;
+          resolve()
+    }, 200);
+  }, Math.random() * 200);
+}
+
+    )};
+
+function incrementSequentially(){
+    incrementCounterJulieta()
+        .then(()=> incrementCounterJulieta()) 
+        .then(()=> incrementCounterJulieta())
+        .then(()=> incrementCounterJulieta()) 
+        .then(()=>console.log(`Listo: ${counter}`))
+}
+incrementSequentially();*/
