a8dcbccfcee2e0df2ccaa5f42bdadc1b80158498
Luciano <Luciano.gonzalez@gm2.com>
2026-08-05 12:27:51 -0300

    feat(exercises2.js)add:exercise 2 completed

--- race-conditions/exercise2.js
+++ race-conditions/exercise2.js
@@ -1,1 +1,28 @@
+let contador = 0
+function updateBalanceLuciano(delta) {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                contador += delta;
+                resolve(contador);
+            }, Math.random() * 200);
+        });
+    }
 
+    async function secuencialLuciano() {
+        const iteraciones = [1,2,3,4];
+        for (const iteracion of iteraciones) {
+            await updateBalance(10);
+        }
+        console.log(contador);
+    }
+
+    async function paraleloLuciano(){
+        variables=[10,10,10,10]
+        await Promise.all(variables.map(variables=>updateBalanceLuciano(variables)))
+        console.log(contador)
+        
+
+    }
+
+secuencialLuciano()
+paraleloLuciano()
