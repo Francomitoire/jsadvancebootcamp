a50a49cdf78ef6c1e00c5fc1176c4febe070b931
tobiasmanquez <tobias.manquez@gm2dev.com>
2026-08-05 10:36:11 -0300

    refactor: comment out counter and related functions in exercise1.js

--- race-conditions/exercise1.js
+++ race-conditions/exercise1.js
@@ -1,23 +1,23 @@
-let counter = 0;
+// let counter = 0;
 
-function incrementCounterBarua() {
-    return new Promise
-    ((resolve) => {
-        setTimeout(() => {
-            const currentValue = counter;
-            setTimeout(() => {
-                counter = currentValue + 1;
-                resolve(counter);
-            }, 200);
-        }, Math.random() * 200);
-    });
-}
+// function incrementCounterBarua() {
+//     return new Promise
+//     ((resolve) => {
+//         setTimeout(() => {
+//             const currentValue = counter;
+//             setTimeout(() => {
+//                 counter = currentValue + 1;
+//                 resolve(counter);
+//             }, 200);
+//         }, Math.random() * 200);
+//     });
+// }
 
-async function incrementSequentiallyBarua(times = 4) {
-    for (let i = 0; i < times; i += 1) {
-        await incrementCounterBarua();
-    }
-}
+// async function incrementSequentiallyBarua(times = 4) {
+//     for (let i = 0; i < times; i += 1) {
+//         await incrementCounterBarua();
+//     }
+// }
 
 incrementSequentiallyBarua().then(() => {
     console.log(`Listo: ${counter}`);
