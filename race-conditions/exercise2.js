21b041a257d313004366fa8f7a3cbc97c98ddacf
santiagobarua-glitch <santiago.barua@gm2dev.com>
2026-08-05 12:26:13 -0300

    feat: add exercise2 to main (#38)
    * feat: add SantiagoBaruaExercise2 with sequential and parallel balance updates
    
    * feat: update SantiagoBaruaExercise2 amounts as a global parameter
    
    * fix: ensure SantiagoBaruaExercise2 is called with a newline at the end of the file

--- race-conditions/exercise2.js
+++ race-conditions/exercise2.js
@@ -1,1 +1,29 @@
+function SantiagoBaruaExercise2(amounts = [12, 5, 5, 18]) {
+    let balance = 0;
 
+    function updateBalance(delta) {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                balance += delta;
+                resolve(balance);
+            }, Math.random() * 200);
+        });
+    }
+
+    async function runSequentiallyBarua() {
+        for (const amount of amounts) {
+            await updateBalance(amount);
+        }
+        console.log('Balance 1:', balance);
+    }
+
+    async function runParallelBarua() {
+        balance = 0;
+        await Promise.all(amounts.map((amount) => updateBalance(amount)));
+        console.log('Balance 2:', balance);
+    }
+
+    runSequentiallyBarua().then(() => runParallelBarua());
+}
+
+SantiagoBaruaExercise2();
