f37571165d17bf11ca8c86a1336baff164053b70
julietainsaurraldegm2 <julieta.insaurralde@gm2dev.com>
2026-08-05 12:01:20 -0300

    feat(exercise2.js): implement race condition example with balance updates

--- race-conditions/exercise2.js
+++ race-conditions/exercise2.js
@@ -1,1 +1,30 @@
+function JulietaExercise2() {
+    let balance = 0;
 
+    function updateBalance(delta) {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                const currentValue = balance;
+
+                setTimeout(() => {
+                    balance = currentValue + delta;
+                    resolve();
+                }, 200);
+
+            }, Math.random() * 200);
+        });
+    }
+
+    async function contarSecuencialmente() {
+        const llamados = [1, 2, 3, 4];
+
+        for (const llamado of llamados) {
+            await updateBalance(10);
+        }
+        console.log(balance);
+    }
+
+    contarSecuencialmente();
+}
+
+JulietaExercise2();
