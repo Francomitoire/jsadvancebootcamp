5481f165cc2ebec416a1ed1e92a17b23d3627bb0
santiagobarua-glitch <santiago.barua@gm2dev.com>
2026-08-05 10:31:02 -0300

    Exercise1 s barua counter (#28)
    * feat: implement counter increment functionality with promises, exercise1.
    
    * chore: rename of functions
    
    * refactor: rename increment functions for clarity

--- race-conditions/exercise1.js
+++ race-conditions/exercise1.js
@@ -1,19 +1,43 @@
-//Mayra Cueto
-let counterMay = 0;
-function incrementCounterMay() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-        const currentValue = counterMay;
+let counter = 0;
+
+function incrementCounterBarua() {
+    return new Promise
+    ((resolve) => {
         setTimeout(() => {
-            counterMay = currentValue + 1;
-            resolve();
-            }, Math.random() * 200)
-    })
-  })
+            const currentValue = counter;
+            setTimeout(() => {
+                counter = currentValue + 1;
+                resolve(counter);
+            }, 200);
+        }, Math.random() * 200);
+    });
 }
 
-incrementCounterMay()
-    .then (() => incrementCounterMay())
-    .then (() => incrementCounterMay())
-    .then (() => incrementCounterMay())
-    .then (() => console.log(counterMay));
+async function incrementSequentiallyBarua(times = 4) {
+    for (let i = 0; i < times; i += 1) {
+        await incrementCounterBarua();
+    }
+}
+
+incrementSequentiallyBarua().then(() => {
+    console.log(`Listo: ${counter}`);
+});
+//Mayra Cueto
+//let counterMay = 0;
+//function incrementCounterMay() {
+ // return new Promise((resolve) => {
+ //   setTimeout(() => {
+ //       const currentValue = counterMay;
+ //       setTimeout(() => {
+ //           counterMay = currentValue + 1;
+ //           resolve();
+ //           }, Math.random() * 200)
+ //   })
+ // })
+//}
+
+//incrementCounterMay()
+//    .then (() => incrementCounterMay())
+//    .then (() => incrementCounterMay())
+//    .then (() => incrementCounterMay())
+//    .then (() => console.log(counterMay));
