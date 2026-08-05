69203c60db36d50998a5eee4434ec4459c8029c6
mayragm2 <mayracueto@gm2dev.com>
2026-08-05 13:01:20 -0300

    feat(exercise2): add paralel and sequential function execution to test promise response

--- race-conditions/exercise2.js
+++ race-conditions/exercise2.js
@@ -1,1 +1,33 @@
+async function mayraExercise2 (){  
+    let balance = 0;
+    let balanceAnterior = 0;
 
+    function updateBalance (delta, order){
+        return new Promise((resolve) => {
+        setTimeout(() => {
+            balanceAnterior = balance;
+            setTimeout(() => {
+                balance = balanceAnterior + delta;
+                resolve();
+                console.log (balanceAnterior)
+                console.log(order);
+                }, Math.random() * 200)
+        }, 200 * Math.random())
+     })       
+    }
+
+    
+    Promise.all ([updateBalance(10, 1), updateBalance(15, 2), updateBalance(15, 3), updateBalance(0, 4)])
+    .then (()=> console.log(`Balance promise: ${balance}`));
+
+
+    for (let i= 0; i<4;i++){
+        (async () => {
+            await updateBalance(10, 5);
+            await console.log (`Balance ${i}: ${balance}`); 
+        }) ();
+    }
+
+}
+
+mayraExercise2();
