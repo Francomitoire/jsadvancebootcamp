6f0a9600702818f841572390f7e8812472418d0c
julietainsaurraldegm2 <julieta.insaurralde@gm2dev.com>
2026-08-06 09:35:40 -0300

    Revert "feat(exercise2.js): implement race condition example with balance upd…"

--- race-conditions/exercise2.js
+++ race-conditions/exercise2.js
@@ -1,39 +1,15 @@
-function JulietaExercise2() {
 function SantiagoBaruaExercise2(amounts = [12, 5, 5, 18]) {
     let balance = 0;
 
     function updateBalance(delta) {
         return new Promise((resolve) => {
             setTimeout(() => {
-                const currentValue = balance;
-
-                setTimeout(() => {
-                    balance = currentValue + delta;
-                    resolve();
-                }, 200);
-
                 balance += delta;
                 resolve(balance);
             }, Math.random() * 200);
         });
     }
 
-    async function contarSecuencialmente() {
-        const llamados = [10, 10, 10, 10];
-
-        for (const delta of llamados) {
-            await updateBalance(delta);
-        }
-
-        return balance;
-    }
-
-    return contarSecuencialmente();
-}
-
-const promesas = [JulietaExercise2()];
-Promise.all(promesas)
-    .then(([balance]) => console.log(balance));
     async function runSequentiallyBarua() {
         for (const amount of amounts) {
             await updateBalance(amount);
