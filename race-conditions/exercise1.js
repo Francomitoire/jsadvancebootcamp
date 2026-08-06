0f8db231f222efe29cb2a37dd51bd9f37aeb1fb0
JeremiasSosaa <jeremias.sosa@gm2dev.com>
2026-08-06 09:13:47 -0300

    Chore/Exercise 2 complete-Sosa

--- race-conditions/exercise1.js
+++ race-conditions/exercise1.js
@@ -3,23 +3,23 @@ let counter = 0;
 function incrementCounterJere() {
     return new Promise((resolve) => {
         setTimeout(() => {
-            const currentValueJere = counterJere;
+            const currentValueJere = balanceParaleloJere;
             setTimeout(() => {
-                counterJere = currentValueJere + 1;
+                balanceParaleloJere = currentValueJere + 1;
                 resolve();
             }, 200);
         }, Math.random() * 200);
     });
 }
 
-async function incrementSequentiallyJere(numberJere) {
+async function contadorBalanceJere(numberJere) {
     for (let i = 0; i < numberJere; i++) {
         await incrementCounterJere();
     }
-    console.log(`Listo: ${counterJere}`);
+    console.log(`Listo: ${balanceParaleloJere}`);
 }
 
-incrementSequentiallyJere(4);
+contadorBalanceJere(4);
 
 /*
 // Tobias Manquez
